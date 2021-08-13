d01fdf19a9b8fe5223c39fff26789beb9c3df31d
trambak.pawar <trambakpawar19@gmail.com>
2021-08-13 19:51:45 +0530

    master

--- cypress/integration/features/school/level.feature
+++ cypress/integration/features/school/level.feature
@@ -4,3 +4,5 @@ Scenario: Add level to school
  Given I click on the setup page
   When I click on grade level and fill the data of level
   Then Grade level get added to school portal
+
+  
--- cypress/integration/pageObjects/school/periods.js
+++ cypress/integration/pageObjects/school/periods.js
@@ -12,7 +12,16 @@ export class period {
 
     addperiod() {
         cy.iframe('.responsive-iframe').find(periodpage).should("have.text", "Marking Periods").eq(0).click()
-        cy.iframe('.responsive-iframe').find(checkbox).should('have.value', 'Y').eq(0).check()
+
+        cy.enter('.responsive-iframe').then(getbody => {
+            getbody().contains('input[name="tables[18][TITLE]"]').clear()
+        })
+
+
+        cy.iframe('.responsive-iframe').find('input[name="tables[18][DOES_COMMENTS]"]').eq(0).check()
+        cy.iframe('.responsive-iframe').find('input[name="tables[18][DOES_GRADES]"]').eq(0).check()
+        cy.iframe('.responsive-iframe').find('input[id="date_3"]').eq(0).type(pdata.postingstart)
+        cy.iframe('.responsive-iframe').find('input[id="date_4"]').eq(0).type(pdata.postingend)
         cy.wait(1000)
 
         cy.iframe('.responsive-iframe').find(savebutton).should("be.visible").click()
